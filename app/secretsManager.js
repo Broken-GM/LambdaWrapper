cdff561859b0b87624db188b1932e794dea4f7e2
Carson-Key <keyc@oregonstate.edu>
2023-11-22 22:27:34 -0800

    Add: SecretsManager helper functins

---
+++ app/secretsManager.js
@@ -0,0 +1,25 @@
+import { GetSecretValueCommand, SecretsManagerClient } from "@aws-sdk/client-secrets-manager";
+
+export const getSecret = async ({ secretName, secrets }) => {
+    const client = new SecretsManagerClient();
+
+    const response = await client.send(
+        new GetSecretValueCommand({
+            SecretId: secretName,
+        }),
+    );
+
+    if (response.SecretString) {
+        secrets[secretName] = response.SecretString;
+    }
+
+    if (response.SecretBinary) {
+        secrets[secretName] = response.SecretBinary;
+    }
+}
+
+const secretsExportsObject = {
+    getSecret
+}
+
+export default secretsExportsObject
