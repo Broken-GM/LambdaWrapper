9fb9ddaef26c22acc25020a7d13af34e32069d15
Carson-Key <carsonkey99@gmail.com>
2025-06-27 10:16:49 -0700

    Update: all dependencies

--- src/appsync/index.ts
+++ src/appsync/index.ts
@@ -1,8 +1,8 @@
 import Logger from "../logger";
-import { SignatureV4 } from "@aws-sdk/signature-v4";
+import { SignatureV4 } from "@smithy/signature-v4";
 import { defaultProvider } from "@aws-sdk/credential-provider-node";
 import { Sha256 } from "@aws-crypto/sha256-js";
-import { HttpRequest } from "@aws-sdk/protocol-http";
+import { HttpRequest } from "@smithy/protocol-http";
 import Ssm from "../ssm";
 import MetaData from "../metaData";
 
